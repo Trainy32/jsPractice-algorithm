87c91ad3de26eb4b8e5a1bb553503953f22c2073
Trainy32 <cjstkdthsu98@naver.com>
2022-05-13 15:40:57 +0900

    1일차 공부 : 기초반 문제 1-4번

---
+++ lecture_basics/variable.js
@@ -0,0 +1,11 @@
+let name = 'suhyun'
+console.log(name)
+
+name = 'good'
+console.log(name)
+
+const doing = 'well'
+console.log(doing)
+
+// doing = 'bad'  const에는 값을 다시 할당할 수 없기 때문에 실행 불가.
+// console.log(doing)
