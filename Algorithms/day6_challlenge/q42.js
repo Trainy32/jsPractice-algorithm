9f1a4b95f28d9b303839290ed4afd92c20f54779
Trainy32 <cjstkdthsu98@naver.com>
2022-05-22 00:11:12 +0900

    1일1문제풀기(2) 약수의 합 (자바스크립트/프로그래머스Lv.1)

--- Algorithms/day6_challlenge/q42.js
+++ Algorithms/day6_challlenge/q42.js
@@ -1,6 +1,14 @@
 // 약수의 합 https://programmers.co.kr/learn/courses/30/lessons/12928
 
+
+let n = 26
 let answer = 0
+
+for (i=0; i<=n; i++) {
+  if (n%i === 0) {
+    answer += i
+  }
+}
 
 console.log(answer)
 
