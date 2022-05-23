67c5aaddae68a4a323fb9543f7484fff3b2e8346
Trainy32 <cjstkdthsu98@naver.com>
2022-05-23 22:38:01 +0900

    1일1문제풀기(3&4) 예산, 최대공약수와 최소공배수 (자바스크립트/프로그래머스Lv.1)

--- Algorithms/day6_challlenge/q43.js
+++ Algorithms/day6_challlenge/q43.js
@@ -1,6 +1,23 @@
 // 예산 https://programmers.co.kr/learn/courses/30/lessons/12982
 
+
+let d = [1,3,2,5,4]
+let budget = 9
 let answer = 0
+
+//먼저 오름차순 정렬해서 작은 수 부터 빼게 만들고
+d = d.sort((a,b) => (a-b))
+
+ 
+ for (i=0; i<d.length; i++) { //부서 숫자만큼 반복해서
+   budget -= d[i] //예산에서 신청한 금액을 빼되,
+
+   if (budget<0) { break; } //예산이 0보다 작아지면 강제로 멈추고
+   else { answer++ } //예산이 남았다면 정답 카운트를 올린다
+ }
+
+ console.log(d)
+
 
 console.log(answer)
 
--- Algorithms/day6_challlenge/q44.js
+++ Algorithms/day6_challlenge/q44.js
@@ -1,7 +1,33 @@
 // 최대공약수와 최소공배수 https://programmers.co.kr/learn/courses/30/lessons/12940
 
+let n = 2
+let m = 5
 
-let answer = 0
+// 두 자연수의 곱은 최대공약수 * 최소 공배수 이다. 따라서, 둘 중 하나를 먼저 구해본다.
+// 일단은 최대공약수를 먼저 구해보기로 했다 :)
+
+let temp_n = n
+let temp_m = m
+
+let answer = []
+
+for (i=n; i>0; i--) {  // 한 숫자에서 시작해서 1씩 줄여가며 반복
+  if( m % i === 0 && n % i === 0) { // 최대 공약수를 발견하면
+    console.log(i)
+    answer.push(i) // 정답배열에 넣고
+    break; // 바로 반복문을 종료해요
+  }
+}
+
+answer.push(n*m/answer[0])  // 두 수를 곱한것을 최대공약수로 나누면 최소공배수를 구할 수있음
 
 console.log(answer)
 
+
+// 다른사람답변
+
+// function greatestCommonDivisor(a, b) {return b ? greatestCommonDivisor(b, a % b) : Math.abs(a);}
+// function leastCommonMultipleOfTwo(a, b) {return (a * b) / greatestCommonDivisor(a, b);}
+// function gcdlcm(a, b) {
+//     return [greatestCommonDivisor(a, b),leastCommonMultipleOfTwo(a, b)];
+// }
