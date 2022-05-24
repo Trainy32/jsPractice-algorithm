18fc266ad9f5e2c5db30e3772cb104d78fc51696
Trainy32 <cjstkdthsu98@naver.com>
2022-05-24 21:17:00 +0900

    1일1문제풀기(5) 나머지가 1이 되는 수 찾기 (자바스크립트/프로그래머스Lv.1)

--- Algorithms/day6_challlenge/q46.js
+++ Algorithms/day6_challlenge/q46.js
@@ -1,6 +1,15 @@
 // 나머지가 1이 되는 수 찾기 https://programmers.co.kr/learn/courses/30/lessons/87389
 
+let n = 12
 let answer = 0
+
+
+for (i=0; i<n; i++) {
+  if ( n%i === 1 ) {
+    answer = i
+    break
+  }
+}
 
 console.log(answer)
 
