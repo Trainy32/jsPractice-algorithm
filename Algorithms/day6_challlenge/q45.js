91585685490ce7d893f63675dabc37401d13a230
Trainy32 <cjstkdthsu98@naver.com>
2022-05-26 02:20:06 +0900

    1일1문제풀기(6) K번째 수 (자바스크립트/프로그래머스Lv.1)

--- Algorithms/day6_challlenge/q45.js
+++ Algorithms/day6_challlenge/q45.js
@@ -1,6 +1,17 @@
 // K번째수 https://programmers.co.kr/learn/courses/30/lessons/42748
 
-let answer = 0
+let array = [1, 5, 2, 6, 3, 7, 4]
+let commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+
+let answer = []
+
+for (i=0; i<commands.length ;i++) {  //커맨드 숫자만큼 수행
+  let temp = array.slice(commands[i][0]-1, commands[i][1]);
+  temp.sort((a,b) => (a-b))
+  answer.push(temp[commands[i][2]-1]);
+} 
+
+
 
 console.log(answer)
 
