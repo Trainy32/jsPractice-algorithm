83e5567b717d201b36cd18df243b7773db99bf96
Trainy32 <cjstkdthsu98@naver.com>
2022-05-27 13:04:22 +0900

    1일1문제풀기(7) 소수 찾기 (자바스크립트/프로그래머스Lv.1)

--- Algorithms/day6_challlenge/q45.js
+++ Algorithms/day6_challlenge/q45.js
@@ -5,10 +5,15 @@ let commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
 
 let answer = []
 
-for (i=0; i<commands.length ;i++) {  //커맨드 숫자만큼 수행
-  let temp = array.slice(commands[i][0]-1, commands[i][1]);
-  temp.sort((a,b) => (a-b))
-  answer.push(temp[commands[i][2]-1]);
+
+// 커맨드 배열 구조 : [[시작숫자, 끝낼숫자, 정렬 후에 몇번째 값 뽑을건지 ][…][…]]
+// 시작 숫자 : 인덱스 번호가 아니다! 1부터 시작한다!
+
+for (i=0; i<commands.length ;i++) {  //주어진 커맨드 배열 숫자만큼 수행
+  //array를 슬라이스해준다. 인덱스는 0부터 시작하니 slice의 시작 인덱스에서는 -1을 해줬다.
+  let temp = array.slice(commands[i][0]-1, commands[i][1]); //우선 임시 배열에 넣어둔상태로
+  temp.sort((a,b) => (a-b)) // 예쁘게 정렬해주고
+  answer.push(temp[commands[i][2]-1]); // 정답 배열에 밀어넣는다!
 } 
 
 
--- Algorithms/day6_challlenge/q47.js
+++ Algorithms/day6_challlenge/q47.js
@@ -1,6 +1,38 @@
 // 소수 찾기 https://programmers.co.kr/learn/courses/30/lessons/12921
 
+
+let n = 121
 let answer = 0
+
+let temp = []
+let prime = true
+
+for(i=2; i <= Math.sqrt(n); i++) {
+  for(j=2; j<i; j++) {
+  prime = true
+    if(i % j === 0){
+      prime = false
+      break;
+    } 
+  }
+  if (prime === true) {
+    temp.push(i)
+  }
+}
+
+for(i=2; i<=n; i++) {
+  prime = true
+  for(j=0; j<temp.length; j++){
+    if(i%temp[j] === 0 && i !== temp[j]) {
+      prime = false
+      break;
+    }
+  }
+  if (prime === true) {
+    answer++
+  }
+}
+  
 
 console.log(answer)
 
