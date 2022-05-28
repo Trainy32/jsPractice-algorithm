d72229cbeecd3e426b05b325871f8f0e559a4b29
Trainy32 <cjstkdthsu98@naver.com>
2022-05-29 00:15:00 +0900

    1일1문제풀기(9) 체육복 (자바스크립트/프로그래머스Lv.1)

--- Algorithms/day6_challlenge/q49.js
+++ Algorithms/day6_challlenge/q49.js
@@ -1,6 +1,40 @@
 // 체육복 https://programmers.co.kr/learn/courses/30/lessons/42862
 
-let answer = 0
+let n = 10
+let lost =  [1,2,3,10]
+let reserve = [5,7]
+let left =[]
+answer = 0
+
+lost = lost.map((student) => (student = [student, -1]) )
+reserve = reserve.map((student) => (student = [student, +1]))
+sum = [...reserve,...lost].sort((a,b) => a[0]-b[0])
+
+for (i=0; i < sum.length; i++) {
+  if (sum[i][1] <= 0) {
+    continue
+
+  } else if (i < sum.length-1 && sum[i][0] === sum[i+1][0]){
+    sum[i][1] = 0
+    sum[i+1][1] = 0
+  } else if (i > 0 && sum[i-1][1] === -1 && sum[i][0]-1 === sum[i-1][0]) {
+    sum[i][1] = 0
+    sum[i-1][1] = 0
+  } else if (i < sum.length-1 && sum[i+1][1] === -1  && sum[i][0]+1 === sum[i+1][0]) {
+    sum[i][1] = 0
+    sum[i+1][1] = 0
+  } 
+}
+answer = n - (sum.filter((student)=> student[1] === -1)).length
+
+// let answer = n - lost.length
+// for (student of lost) {
+//   let temp = reserve.map((cloths)=>{ cloths= ?  : cloths = 0 })
+
+// }
+
+
+
 
 console.log(answer)
 
---
+++ Algorithms/day6_challlenge/rough.js
@@ -0,0 +1,9 @@
+const arr = [1, 2, 3, 4, 5]
+
+console.log(arr)
+
+const [a, b, c, d, e] = arr
+
+console.log(a)
+
+console.log(arr)
