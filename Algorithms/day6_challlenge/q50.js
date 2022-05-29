938f8c8a7ae5bef8458a34659f38cc4794c859cd
Trainy32 <cjstkdthsu98@naver.com>
2022-05-30 02:06:47 +0900

    1일1문제풀기(10) 폰켓몬 (자바스크립트/프로그래머스Lv.1)

--- Algorithms/day6_challlenge/q50.js
+++ Algorithms/day6_challlenge/q50.js
@@ -1,6 +1,11 @@
 // 폰켓몬 https://programmers.co.kr/learn/courses/30/lessons/1845
 
+let nums = [3,1,2,3]
 let answer = 0
 
+const pTypes = new Set(nums)
+answer = nums.length/2 < pTypes.size ? nums.length/2 : pTypes.size
+
 console.log(answer)
+
 
--- Algorithms/day6_challlenge/rough.js
+++ Algorithms/day6_challlenge/rough.js
@@ -1,9 +1,26 @@
-const arr = [1, 2, 3, 4, 5]
+class User {
+  constructor(email, birthdate) {
+    this.email = email;
+    this.birthdate = birthdate;
+  }
 
-console.log(arr)
+  buy(item) {
+    console.log(`${this.email} buys ${item.name}`);
+  }
 
-const [a, b, c, d, e] = arr
+  get email() {
+    return this._email;
+  }
 
-console.log(a)
+  set email(address) {
+    if (address.includes('@')) {
+      this._email = address;
+    } else {
+      throw new Error('invalid email address');
+    }
+  }
+}
 
-console.log(arr)
+const user1 = new User('chris123@google.com', '1992-03-21');
+user1.email = 'newChris123@google.com';
+console.log(user1.email);
