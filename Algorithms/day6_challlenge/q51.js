5b98c088008acf2ccc92c931866bced0aeed4e2e
Trainy32 <cjstkdthsu98@naver.com>
2022-05-31 03:24:48 +0900

    1일1문제풀기(11) [1차] 비밀지도 (자바스크립트/프로그래머스Lv.1)

--- Algorithms/day6_challlenge/q51.js
+++ Algorithms/day6_challlenge/q51.js
@@ -1,6 +1,23 @@
 //  [1차] 비밀지도 https://programmers.co.kr/learn/courses/30/lessons/17681
 
-let answer = 0
+let arr1 = [46, 33, 33 ,22, 31, 50]
+let arr2 = [27 ,56, 19, 14, 14, 10]
+
+let answer = []
+
+let rowLength = Math.max(...arr1,...arr2).toString(2).split('').length
+arr1 = arr1.map((rows) => rows.toString(2).split(''))
+arr2 = arr2.map((rows) => rows.toString(2).split(''))
+
+for (i=0; i<arr1.length; i++) {
+  if (arr1[i].length < rowLength) { arr1[i].unshift(...'0'.repeat((rowLength - arr1[i].length)).split('')) }
+  if (arr2[i].length < rowLength) { arr2[i].unshift(...'0'.repeat(rowLength - arr2[i].length).split('')) }
+
+  answer.push(arr1[i].map((blocks, j) => { 
+    blocks = blocks*1 + arr2[i][j]*1;
+    return blocks > 0 ? '#' : ' ' } ).join(''))
+}
 
 console.log(answer)
+
 
